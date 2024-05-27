24d4b5e3c5d38c97be8613161150e5d48fcd1b40
IlluminateDreamer <kabeercybersec@gmail.com>
2024-05-27 07:59:19 -0400

    Bash Scripting Notes

---
+++ BashScripting/input.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo "What's your name?: "
+read name
+
+echo "Hi $name!"
+
+# another method to take input from user is
+
+read -p "What's your name?:" name
+echo "Hi $name!"
+
+# there are lot of other methods but i listed only few of them
+
--- BashScripting/print.sh
+++ BashScripting/print.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
 echo "Hello Friend!"
+printf "The value of pi is: %.2f\n" 3.14159
+echo -e "Hello\nWorld"
+
+
+# we don't need to remember everything
