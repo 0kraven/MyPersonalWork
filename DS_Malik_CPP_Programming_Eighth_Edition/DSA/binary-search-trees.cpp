e2499729a5e80114efe6b9b2d0a17857f4932b87
IlluminateDreamer <kabeercybersec@gmail.com>
2024-11-14 16:46:29 +0500

    DataStructures

---
+++ DS_Malik_CPP_Programming_Eighth_Edition/DSA/binary-search-trees.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+struct node{
+    int data;
+    TNode* left;
+    TNode* right;
+}
+
+class TNode{
+    private:
+        
+    public:
+
+}
+
+int main(int argc, char) {
+
+}
