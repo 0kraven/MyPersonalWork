fdc9ad23b4506e1be0dadf283125fb40b3f12afe
IlluminateDreamer <kabeercybersec@gmail.com>
2024-11-28 22:00:33 +0500

    just practicing tree by myself.

--- DataStructures/binary-search-trees.cpp
+++ DataStructures/binary-search-trees.cpp
@@ -1,94 +1,53 @@
 #include<iostream>
 using namespace std;
 
-struct nodeTree {
+struct node{
     int data;
-    nodeTree* left;
-    nodeTree* right;
-    nodeTree(int data) : data(data), left(nullptr), right(nullptr) {}
-}
+    node* left;
+    node* right;
+    node(int data = 0, node* left = nullptr, node* right = nullptr) : data(data), left(left), right(right) {}
+};
+
 class BSTree{
     private:
-        nodeTree* root;
-        nodeTree* insertHelper(nodeTree* node, int data){
-            if(node == nullptr) return new nodeTree(data);
-            if(data < node->data) node->left = insertHelper(node->left, data);
-            else if(data > node->data) node->right = insertHelper(node->right, data);
-            return node;
-        }
-        void inorderTraversalHelper(nodeTree* node){
-            if(node!=nullptr){
-                inorderTraversalHelper(node->left);
-                cout << node->data << " ";
-                inorderTraversalHelper(node->right);
+        node* root;
+        node* insert_helper(node* Node, int data = 0) {
+            if(!Node) {
+                return new node(data);
             }
-        }
-        void preorderTraversalHelper(nodeTree* node){
-            if(node!=nullptr){
-                cout << node->data << " ";
-                preorderTraversalHelper(node->left);
-                preorderTraversalHelper(node->right);
+            else if(Node->data >= data) {
+                Node->left = insert_helper(Node->left, data);
             }
-        }
-        void postorderTraversalHelper(nodeTree* node){
-            if(node!=nullptr){
-                postorderTraversalHelper(node->left);
-                postorderTraversalHelper(node->right);
-                cout << node->data << " ";
+            else if(Node->right == nullptr) {
+                Node->right = insert_helper(Node->right,data);
             }
+            return Node;
         }
-        bool searchHelper(nodeTree* node, int data){
-            if(node == nullptr) return false;
-            if(data < node->data) return searchHelper(node->left, data);
-            else if(data > node->data) return searchHelper(node->right, data);
-            return true;
-        }
-        nodeTree* minValueNodeHelper(nodeTree* node){
-            while(node && node->left!=nullptr) node = node->left;
-            return node;
-        }
-        nodeTree* deleteNodeHelper(nodeTree* root, int data){
-            if(root == nullptr) return root;
-            if(data < root->data) root->left = deleteNodeHelper(root->left, data);
-            else if(data > root->data) root->right = deleteNodeHelper(root->right, data);
-            else{
-                if(root->left == nullptr) return root->right;
-                else if(root->right == nullptr) return root->left;
-                nodeTree* temp = minValueNodeHelper(root->right);
-                root->data = temp->data;
-                root->right = deleteNodeHelper(root->right, temp->data);
+        void inorder_helper(node* node) {
+            if( node == nullptr ) {
+                return;
             }
-            return root;
+            inorder_helper(node->left);
+            cout << node->data << " ";
+            inorder_helper(node->right);
         }
-        
-
     public:
-        BSTree() : root(nullptr) {}
-        void insert(int data){
-            root = insertHelper(root, data);
-        }
-        void displayInorder(){
-            inorderTraversalHelper(root);
-        }
-        void displayPreorder(){
-            preorderTraversalHelper(root);
-        }
-        void displayPostorder(){
-            postorderTraversalHelper(root);
-        }
-        bool search(int data){
-            return searchHelper(root, data);
-        }
-        void remove(int data){
-            root = deleteNodeHelper(root, data);
-        }
-        void display(){
-            cout << "Inorder traversal: ";
-            displayInorder();
-            cout << "\nPreorder traversal: ";
-            displayPreorder();
-            cout << "\nPostorder traversal: ";
-            displayPostorder();
-        }
         
+        BSTree(node* root = nullptr) : root(root) {}
+        void insert(int data = 0){
+            root = insert_helper(root,data);
+        }
+        void inorder() {
+            inorder_helper(root);
+        }
+};
+int main() {
+
+    BSTree root;
+    root.insert(50);
+    root.insert(30);
+    root.insert(20);
+    root.insert(40);
+    root.insert(70);
+    root.inorder();
 }
