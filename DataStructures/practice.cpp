dc54b5a6f43d64e39e810cc1a28c77734ff9393d
IlluminateDreamer <kabeercybersec@gmail.com>
2024-12-01 10:58:49 +0500

    just practicing tree by myself.

--- DataStructures/practice.cpp
+++ DataStructures/practice.cpp
@@ -1,34 +1,101 @@
 #include<iostream>
+#include<queue>
 using namespace std;
 
-struct node{
+struct Node{
     int data;
-    node* left;
-    node* right;
-    node(int data = 0, node* left = 0, node* right = 0) : data(data), left(left), right(right) {}
+    Node* left;
+    Node* right;
+    Node(int data = 0, Node* left = 0, Node* right = 0) : data(data), left(left), right(right) {}
 };
 
 class Tree{
     private:
-        node* root;
+        Node* root;
         
-        node* insert_helper(node* Node  = 0, int data = 0) {
-            if(Node == 0) return new node(data);
-            if(data < node->data) node->left = insert_helper(Node->left, data);
-            else node->right = insert_helper(Node->right, data);
+        Node* insert_helper(Node* node  = 0, int data = 0) {
+            if(node == 0) return new Node(data);
+            if(data < node->data) node->left = insert_helper(node->left, data);
+            else node->right = insert_helper(node->right, data);
             return node;
+        }
+
+        // Pre-order
+        void preorder_helper(Node* root) {
+            if (root == nullptr) return;
+            cout << root->data << " ";
+            preorder_helper(root->left);
+            preorder_helper(root->right);
+        }
+
+        // In-order
+        void inorder_helper(Node* root) {
+            if (root == nullptr) return;
+            inorder_helper(root->left);
+            cout << root->data << " ";
+            inorder_helper(root->right);
+        }
+
+        // Post-order
+        void postorder_helper(Node* root) {
+            if (root == nullptr) return;
+            postorder_helper(root->left);
+            postorder_helper(root->right);
+            cout << root->data << " ";
+        }
+        void levelorder_helper(Node* root) {
+            if (root == nullptr) return;
+            queue<Node*> q;
+            q.push(root);
+
+            while (!q.empty()) {
+                Node* current = q.front();
+                q.pop();
+                cout << current->data << " ";
+
+                if (current->left) q.push(current->left);
+                if (current->right) q.push(current->right);
+            }
         }
     public:
         
-        Tree(node* root) : root(root) {}
+        Tree(Node* root = 0) : root(root) {}
 
 
         void insert(int data) {
             root = insert_helper(root, data); // root is only updated when there is no root node
         }
+        void preorder() {
+            preorder_helper(root);
+        }
+        void inorder() {
+            inorder_helper(root);
+        }
+        void postorder() {
+            postorder_helper(root);
+        }
+        void levelorder() {
+            levelorder_helper(root);
+        }
 
 };
 
 int main() {
-
+    Tree root;
+    root.insert(50);
+    root.insert(30);
+    root.insert(20);
+    root.insert(40);
+    root.insert(70);
+    root.insert(60);
+    root.insert(80);
+    cout << "Preorder traversal of the constructed tree is \n";
+    root.preorder();
+    cout << "\nInorder traversal of the constructed tree is \n";
+    root.inorder();
+    cout << "\nPostorder traversal of the constructed tree is \n";
+    root.postorder();
+    cout << "\nLevel order traversal of the constructed tree is \n";
+    root.levelorder();
+    return 0;
 }
