5bee66e860b181324fc92149863d1534482dccb9
IlluminateDreamer <kabeercybersec@gmail.com>
2024-12-07 09:13:58 +0500

    bubble-sort-array-implementation.cpp

--- ALgorithms/bubble-sort-array-implementation.cpp
+++ ALgorithms/bubble-sort-array-implementation.cpp
@@ -1,9 +1,8 @@
 #include<iostream>
 using namespace std;
 
-int main() {
-    int arr[] = {9,8,7,6,5,56,3,2,3,5,4,9,5,4,57,9,0,7,8,34,3,2,1,0};
-    int j = sizeof(arr)/sizeof(arr[0]);
+void bubble_sort(int arr[], int size) {
+    int j = size;
     while(j--) {
         for(int i = 0; i < j; i++) {
             if(arr[i] > arr[i+1]) {
@@ -13,7 +12,13 @@ int main() {
             }
         }
     }
-    for(int i = 0; i < sizeof(arr)/sizeof(arr[i]); i++) {
+}
+
+int main() {
+    int arr[] = {9,8,7,6,5,56,3,2,3,5,4,9,5,4,57,9,0,7,8,34,3,2,1,0};
+    bubble_sort(arr, sizeof(arr)/sizeof(arr[0]));
+    for(int i = 0; i < sizeof(arr)/sizeof(arr[0]); i++) {
         cout << arr[i] << " ";
     }
+    
 }
