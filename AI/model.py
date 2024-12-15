59a931bd4ad53135c20fd96826f2263b3cde875f
IlluminateDreamer <kabeercybersec@gmail.com>
2024-12-15 10:12:26 +0500

    prepration for AI lab final exam.

--- AI/model.py
+++ AI/model.py
@@ -1,6 +1,5 @@
 import panda as pd
 import numpy as np
-import warnings
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score
@@ -13,7 +12,18 @@ heart.tail()
 heart.shape
 heart.info()
 heart.isnull()
+heart.fillna(heart.median(), inplace = True)
 heart.describe()
 heart['target'].value_counts()
+heart['target'].replace({'yes' : 1, 'no' : 0})
+X = heart.drop(columns='target', axis=1)
+Y = heart['target']
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2, random_state=42)
+model = LogisticRegression()
+model.fit(X_train, Y_train)
+y_pred = model.predict(X_test)
+accuracy = accuracy_score(Y_test, y_pred)
+
+
 
 
