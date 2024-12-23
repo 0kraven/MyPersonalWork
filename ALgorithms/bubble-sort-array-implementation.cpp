74f66f1177ce5817d83161200f13d8c92ae61c4c
IlluminateDreamer <kabeercybersec@gmail.com>
2024-12-23 07:34:14 +0500

    just my own stuff.

--- ALgorithms/bubble-sort-array-implementation.cpp
+++ ALgorithms/bubble-sort-array-implementation.cpp
@@ -1,12 +1,12 @@
 #include<iostream>
 using namespace std;
 
-void bubble_sort(int arr[], int size) {
+void bubble(int arr[], int size) {
+    bool swapped = false;
     int j = size;
-    bool swapped;
-    while(j--) {
+    while (j--) {
         swapped = false;
-        for(int i = 0; i < j; i++) {
+        for(int i = 0; i < j; i++){
             if(arr[i] > arr[i+1]) {
                 int temp = arr[i];
                 arr[i] = arr[i+1];
@@ -14,17 +14,16 @@ void bubble_sort(int arr[], int size) {
                 swapped = true;
             }
         }
-        if(!swapped) break;
+        if(!swapped) return;
     }
 }
 
 int main() {
-    int arr[] = {9,8,7,6,5,56,3,2,3,5,4,9,5,4,57,9,0,7,8,34,3,2,1,0};
-    bubble_sort(arr, sizeof(arr)/sizeof(arr[0]));
-    for(int i = 0; i < sizeof(arr)/sizeof(arr[0]); i++) {
+    int arr[] = {9,8,7,6,5,4,3,2,1,0};
+    int size = sizeof(arr)/sizeof(arr[0]);
+    bubble(arr, size);
+    for(int i = 0; i < size; i++) {
         cout << arr[i] << " ";
     }
-    
+    return 0;
 }
-// Adding the swapped flag improves the best-case time complexity of Bubble Sort to O(n). This is beneficial when the array is already sorted or nearly sorted.
-// The outer loop still iterates up to size - 1 times in the worst case, maintaining the overall worst-case time complexity of O(n²).
--- ALgorithms/insertion-sort-array-implementation.cpp
+++ ALgorithms/insertion-sort-array-implementation.cpp
@@ -1,23 +1,20 @@
 #include<iostream>
 using namespace std;
 
-void insertion_sort(int arr[], int size) {
-    for(int i = 1; i < size; i++) { 
+void insertion(int arr[] = {0}, int size = 10) {
+    for(int i = 1; i < size; i++){
         int key = arr[i];
-        int j = i - 1;
-        while(j >= 0 && arr[j] > key){
+        int j = i-1;
+        while(j-- && arr[j] > key)
             arr[j+1] = arr[j];
-            j--;
-        }
         arr[j+1] = key;
-    }    
+    }
 }
 
-int main(){
-    int arr[] = {9, 8, 7, 6, 5, 4, 3, 2, 1, 0};
-    int size = sizeof(arr) / sizeof(arr[0]);
-    insertion_sort(arr, size);
-    for(int i = 0; i < size; i++){
+int main() {
+    int arr[] = {9,8,7,6,5,4,3,2,1,0};
+    insertion(arr, sizeof(arr)/sizeof(arr[0]));
+    for(int i = 0; i < sizeof(arr)/sizeof(arr[0]); i++){
         cout << arr[i] << " ";
     }
 }
